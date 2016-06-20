f7170f6019def2e413717194a4466d1266ad6c4a
Andreea O. Bucurica <wanna_andreea@yahoo.com>
2016-06-20 17:06:09 +0300

    Make devise look better

--- estuk2/config/routes.rb
+++ estuk2/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "/pages/:page" => "pages#show"
-  root "pages#show", page: "home"
+  root 'pages#home'
+    
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
